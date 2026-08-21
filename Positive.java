0b5f3587a5ebf851efa72c9030004f8a7f7ff374
ModelCampus2060 <modelcampusdamak09@gmail.com>
2026-08-21 02:50:48 +0000

    positive

---
+++ Positive.java
@@ -0,0 +1,8 @@
+class Positive{
+    public static void main(String[] args)
+    {
+        System.out.println("Positive Numbers between 50-100:");
+        for(int i=50; i<=100; i++)
+        System.out.println(i);
+    }
+}
