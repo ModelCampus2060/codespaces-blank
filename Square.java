17bd557c6c18bb8412da341a95c65b2dae6ebe2e
ModelCampus2060 <modelcampusdamak09@gmail.com>
2026-08-21 02:53:03 +0000

    Square

--- Square.java
+++ Square.java
@@ -2,7 +2,7 @@ class Square{
     public static void main(String[] args)
     {
         System.out.println("Square Numbers betweeen 1-100:");
-        for(int n=1; n<=100; n++)
+        for(int n=1; n*n<=100; n++)
             System.out.println(n*n);
     }
 }
