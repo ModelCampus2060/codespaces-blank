0a8025dfd117290445584260ecb2aec3946aac3b
ModelCampus2060 <modelcampusdamak09@gmail.com>
2026-08-24 02:37:15 +0000

    eligible

---
+++ Valid.java
@@ -0,0 +1,14 @@
+public class Valid{
+    public static void validate(int age){
+        if(age<18){
+            throw new ArithmeticException("Person is not Eligible to vote");
+        }
+        else{
+            System.out.println("Person is Eligible to vote");
+        }
+    }
+    public static void main(String[] args){
+        validate(26);
+        System.out.println("Rest of the code.........");
+    }
+}
