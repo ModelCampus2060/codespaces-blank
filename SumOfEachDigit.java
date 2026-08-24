a6d834544d2b137164127c21fc535e98f9328c3d
ModelCampus2060 <modelcampusdamak09@gmail.com>
2026-08-24 03:01:26 +0000

    Sumofeachdigit

---
+++ SumOfEachDigit.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+public class SumOfEachDigit {
+    public static void main(String[] args) {
+        int sum=0;
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter any number:");
+        int n=sc.nextInt();
+        while(n!=0)
+        {
+            int rem=n%10;
+            sum+=rem;
+            n=n/10;
+        }
+        System.out.println(sum);
+        sc.close();
+    }
+    
+}
